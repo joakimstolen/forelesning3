0f18ccf979f0701e1497f61c36bc8e00389ad1b1
joakimstolen <joakimstoelen@hotmail.com>
2020-09-07 08:53:51 +0200

    Initial commit

---
+++ src/main/java/com/example/forelesning3/Forelesning3Application.java
@@ -0,0 +1,13 @@
+package com.example.forelesning3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forelesning3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Forelesning3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forelesning3/Forelesning3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forelesning3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forelesning3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

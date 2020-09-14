63bfbec839de2d9babf4e13280407d83ac11740e
joakimstolen <joakimstoelen@hotmail.com>
2020-09-14 12:17:23 +0200

    minor change to docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
-docker build . --tag forelesning4_docker_2 --build-arg JAR_FILE=./target/forelesning3-0.0.1-SNAPSHOT.jar
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build . --tag forelesning4_docker_2 --build-arg JAR_FILE=./target/forelesning3-0.0.1-SNAPSHOT.jar
 docker tag forelesning4_docker_2  joakimstolen/forelesning4_docker_2
 docker push joakimstolen/forelesning4_docker_2
+docker run -p 8080:8080 joakimstolen/forelesning4_docker_2
